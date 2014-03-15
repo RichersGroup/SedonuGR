98159f71cdfe88effd35e9a83dad35eafac3fd82
Sherwood Richers <srichers@tapir.caltech.edu>
2014-03-15 13:12:31 -0700

    fixed up phot_emitting_ball

--- tests/phot_emitting_ball/compare_spectrum.gnuplot
+++ tests/phot_emitting_ball/compare_spectrum.gnuplot
@@ -3,12 +3,16 @@ c = 2.99e10         #cm/s
 h = 6.62606957e-27  #erg/Hz
 sigma = 5.67051e-5  #erg/cm^2/K^4/s
 pi = 3.14159265359
+visc_specific_heat_rate = 5e10
 
 r = 1e5                      #cm
-T = 1e4                      #K
-L = 4*pi/3                   #erg/s
-P = 4*pi*r*r*sigma*T*T*T*T #erg/s
-N = L/P
+rho = 1e-15  #g/ccm
+T = 6.86e3                      #K
+Lvisc = 4./3.*pi*r*r*r*rho * visc_specific_heat_rate #erg/s
+print Lvisc
+Lplanck = 4.*pi*r*r*sigma*T*T*T*T         #erg/s
+print Lplanck
+N = Lvisc/Lplanck                         # normalize BB spectrum to unit net luminosity, then multiply by expected luminosity
 
 set xlabel "Frequency (Hz)"
 set ylabel "Energy Flux (erg/s/Hz)"
@@ -16,9 +20,10 @@ set ylabel "Energy Flux (erg/s/Hz)"
 set xrange [0:2e15]
 set term pdf
 set output "compare_spectrum.pdf"
-plot N*4*pi*pi*r*r*2*x*x*x*h/c/c*1/(exp(h*x/(k_b*T))-1), './species0_I1.spec' using 1:2
+plot N*4*pi*pi*r*r*2*x*x*x*h/c/c*1/(exp(h*x/(k_b*T))-1), './species0_I10.spec' using 1:2
 set output
 
 # planck function has units of erg/s/cm^2/Hz/ster
 # 4*pi is the total solid angle through which photons are collected
 # pi*r*r is the cross-sectional surface area
+# 10 is because the spectrum is integrated over 10 iterations
--- tests/phot_emitting_ball/param.lua
+++ tests/phot_emitting_ball/param.lua
@@ -1,34 +1,37 @@
 -- what are we simulating?
 
-do_photons     = 1
-do_neutrinos   = 0
-solve_T        = 0
-solve_Ye       = 0
-radiative_eq   = 1;   -- set to enforce radiative equilibrium. 
-iterate        = 1;   -- set to do an iterative (time independent) calc
+do_photons   = 1                 -- simulate photons?
+do_neutrinos = 0                 -- simulate neutrinos?
+iterate      = 1                 -- iterative calculation (solve for steady-state configuration)? 
+solve_T      = 1                 -- (if iterative) solves each zone's temperature based on its absorbed energy
+solve_Ye     = 0                 -- (if iterative) solves each zone's Ye based on its absorbed lepton number
+do_visc      = 1
+visc_specific_heat_rate = 5e10      -- specific heating rate due to viscosity (erg/s/g)
 
 -- input/output files
 
 grid_type = "grid_1D_sphere"
 model_file  =  "lucy.mod"
+write_zones_every   = 1
+write_rays_every    = 1
+write_spectra_every = 1
 
 -- spectrum parameters
-spec_n_mu      =    1              -- number of cos(theta) bins in output spectrum 
-spec_n_phi     =    1              -- number of phi bins in output spectrum
-spec_time_grid = {1,1,1}           -- {start, stop, bin width}
-spec_nu_grid   = {0,2e15,2e13}     -- {start, stop, bin width}
+phot_spec_n_mu      =    1              -- number of cos(theta) bins in output spectrum 
+phot_spec_n_phi     =    1              -- number of phi bins in output spectrum
+phot_spec_time_grid = {1,1,1}           -- {start, stop, bin width}
+phot_spec_nu_grid   = {0,2e15,2e13}     -- {start, stop, bin width}
 
 
 -- particle creation parameters
-init_particles = 0                 -- # particles spawned from the pre-existing 'radiation energy' in each zone
 n_emit_core    = 0                 -- # particles to emit from core each timestep
-n_emit_heat    = 0                 -- # particles to emit from zones each timestep ("actual" emission, ignored if radiative_eq)
-n_emit_visc    = 1e4               -- # particles to emit from zones each timestep (from viscosity, ignored if !radiative_eq)
+n_emit_therm   = 1e4                 -- # particles to emit from zones each timestep ("actual" emission, ignored if radiative_eq)
 n_emit_decay   = 0                 -- # particles to emit from zones each timestep (from non-thermal processes)
-max_particles  = 1e6
-visc_specific_heat_rate = 1.0      -- specific heating rate due to viscosity (erg/s/g)
+max_particles = 1e6
 
 -- particle propagation parameters
+max_n_steps = 10
+dt = 1
 step_size = 0.4                    -- move at most step_size*min_grid_length at a time
 
 
@@ -38,11 +41,11 @@ L_core = 0                       -- core luminosity (erg/s)
 T_core = 0                       -- core temperature (K)
 
 -- opacity parameters
-grey_opacity    =  0.1           -- optical grey opacity (cm^2/g)
-epsilon         =  1.0           -- absorption fraction
-nu_start        =  0             -- photon opacity grid start
-nu_stop         =  2e15          -- photon opacity grid stop
-n_nu            =  500           -- photon opacity grid number of points along frequency
+phot_grey_opacity    =  0.1           -- optical grey opacity (cm^2/g)
+phot_epsilon         =  1.0           -- absorption fraction
+phot_nu_start        =  0             -- photon opacity grid start
+phot_nu_stop         =  2e15          -- photon opacity grid stop
+phot_n_nu            =  500           -- photon opacity grid number of points along frequency
 
 
 -- equilibrium solver parameters
