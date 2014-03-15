2850666cbd78377b38db3e40d6d55be0f2f352fa
Sherwood Richers <srichers@tapir.caltech.edu>
2014-03-14 22:40:51 -0700

    updated nut_emitting_ball test

--- tests/nut_emitting_ball/param.lua
+++ tests/nut_emitting_ball/param.lua
@@ -1,44 +1,55 @@
 -- what are we simulating?
 
-do_photons     = 0                 -- simulate photons?
-do_neutrinos   = 1                 -- simulate neutrinos?
-radiative_eq   = 1                 -- set to enforce radiative equilibrium. 
-iterate        = 1                -- set to do an iterative (time independent) calc
-solve_T        = 0                 -- (if radiative_eq) solves each zone's temperature based on its absorbed energy
-solve_Ye       = 0                 -- (if radiative_eq) solves each zone's Ye based on its absorbed lepton number
+do_photons   = 0                 -- simulate photons?
+do_neutrinos = 1                 -- simulate neutrinos?
+iterate      = 1                 -- iterative calculation (solve for steady-state configuration)? 
+solve_T      = 1                 -- (if iterative) solves each zone's temperature based on its absorbed energy
+solve_Ye     = 1                 -- (if iterative) solves each zone's Ye based on its absorbed lepton number
+do_visc      = 1
+visc_specific_heat_rate = 1e22
 
 -- input/output files
 
 grid_type = "grid_1D_sphere"       -- grid geometry. Must match grid geometry in model file if used  
 model_file  =  "neutron_star.mod"  -- model file. "custom" --> use hard coded model
 nulib_table = "../../external/tables/NuLib.h5" -- NuLib opacity/emissivity table
+write_zones_every   = 10
+write_rays_every    = 10
+write_spectra_every = 10
 
 -- spectrum parameters
 
-spec_time_grid   = {1,1,1}         -- spectrum time grid parameters {start,stop,delta} (start==stop --> single bin catch-all)
-spec_nu_grid = {0,3e22,1e20}    -- spectrum frequency grid parameters {start,stop,delta} (start==stop --> single bin catch-all)
-spec_n_mu             = 1          -- number of cos(theta) bins in output spectrum 
-spec_n_phi            = 1          -- number of phi bins in output spectrum
+spec_time_grid  = {1,1,1}          -- spectrum time grid parameters {start,stop,delta} (start==stop --> single bin catch-all)
+spec_nu_grid    = {0,2e22,1e20}    -- spectrum frequency grid parameters {start,stop,delta} (start==stop --> single bin catch-all)
+spec_n_mu       = 1                -- number of cos(theta) bins in output spectrum 
+spec_n_phi      = 1                -- number of phi bins in output spectrum
 
 -- particle creation parameters
 
-init_particles = 0                 -- # particles spawned from the pre-existing 'radiation energy' in each zone
 n_emit_core    = 0                 -- # particles to emit from core each timestep
-n_emit_heat    = 0                 -- # particles to emit from zones each timestep ("actual" emission, ignored if radiative_eq)
-n_emit_visc    = 1e4               -- # particles to emit from zones each timestep (from viscosity, ignored if !radiative_eq)
+n_emit_therm   = 1e5               -- # particles to emit from zones each timestep
 n_emit_decay   = 0                 -- # particles to emit from zones each timestep (from non-thermal processes)
 max_particles  = 1e6
-visc_specific_heat_rate = 1.0
+
+-- particle propagation parameters
+
+max_n_steps = 10
+dt = 1
+step_size = 0.4                    -- move at most step_size*min_grid_length at a time
+
+-- inner source
+
+r_core = 0                         -- core radius (cm)
+L_core = 0                         -- core luminosity (erg/s)
+T_core = 0                         -- core temperature (K)
 
 -- opacity parameters
 
 nut_grey_opacity    =  -1          -- optical grey opacity (cm^2/g)
 nut_epsilon         =  -1          -- absorption fraction
 
--- particle propagation parameters
-
-step_size = 0.4                    -- move at most step_size*min_grid_length at a time
-
 -- equilibrium solver parameters
 
-damping = 0.5                      -- changes in values between iterations are decreased by this factor
+damping = 0.0                      -- changes in values between iterations are decreased by this factor
+brent_itmax = 100
+brent_tolerance = 0.01
