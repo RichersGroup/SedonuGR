434e42b836d62d537798d33b766167a40f267702
Sherwood Richers <srichers@caltech.edu>
2014-08-25 23:00:16 -0700

    updated nut_neutron_star test

--- tests/nut_neutron_star/nut_neutron_star.gnuplot
+++ tests/nut_neutron_star/nut_neutron_star.gnuplot
@@ -1,15 +1,15 @@
 set term pdf
 set grid
-set xrange [:9.999e5]
+set xrange [:9.999]
 
 set xlabel "Radius (km)"
 set ylabel "Y_e"
 set output "r_ye.pdf"
-plot 'fluid_00000' u 1:7 w l, 'fluid_00001' u 1:7 w l, 'fluid_00002' u 1:7 w l, 'fluid_00003' u 1:7 w l, 'fluid_00004' u 1:7 w l, 'fluid_00005' u 1:7 w l, 'fluid_00006' u 1:7 w l, 'fluid_00007' u 1:7 w l, 'fluid_00008' u 1:7 w l, 'fluid_00009' u 1:7 w l
+plot 'fluid_00000.dat' u ($1/100000):5 w l, 'fluid_00001.dat' u ($1/100000):5 w l, 'fluid_00002.dat' u ($1/100000):5 w l, 'fluid_00003.dat' u ($1/100000):5 w l, 'fluid_00004.dat' u ($1/100000):5 w l, 'fluid_00005.dat' u ($1/100000):5 w l, 'fluid_00010.dat' u ($1/100000):5 w l, 'fluid_00015.dat' u ($1/100000):5 w l, 'fluid_00020.dat' u ($1/100000):5 w l
 set output
 
 set xlabel "Radius (km)"
-set ylabel "Temperature (K)"
+set ylabel "Temperature (MeV)"
 set output "r_T.pdf"
-plot 'fluid_00000' u 1:6 w l, 'fluid_00001' u 1:6 w l, 'fluid_00002' u 1:6 w l, 'fluid_00003' u 1:6 w l, 'fluid_00004' u 1:6 w l, 'fluid_00005' u 1:6 w l, 'fluid_00006' u 1:6 w l, 'fluid_00007' u 1:6 w l, 'fluid_00008' u 1:6 w l, 'fluid_00009' u 1:6 w l
+plot 'fluid_00000.dat' u ($1/100000):4 w l, 'fluid_00001.dat' u ($1/100000):4 w l, 'fluid_00002.dat' u ($1/100000):4 w l, 'fluid_00003.dat' u ($1/100000):4 w l, 'fluid_00004.dat' u ($1/100000):4 w l, 'fluid_00005.dat' u ($1/100000):4 w l, 'fluid_00010.dat' u ($1/100000):4 w l, 'fluid_00015.dat' u ($1/100000):4 w l, 'fluid_00020.dat' u ($1/100000):4 w l
 set output
--- tests/nut_neutron_star/param.lua
+++ tests/nut_neutron_star/param.lua
@@ -1,5 +1,6 @@
 -- what are we simulating?
 
+verbose      = 1
 do_photons   = 0                 -- simulate photons?
 do_neutrinos = 1                 -- simulate neutrinos?
 radiative_eq = 1
@@ -13,7 +14,7 @@ reflect_outer= 0
 
 grid_type = "grid_1D_sphere"       -- grid geometry. Must match grid geometry in model file if used  
 model_file  =  "neutron_star.mod"  -- model file. "custom" --> use hard coded model
-nulib_table = "../../external/tables/NuLib_LS220_rho150_temp90_ye60_ng24_ns3_version1.0_20140327.h5" -- NuLib opacity/emissivity table
+nulib_table = "../../external/tables/NuLib_LS220_rho100_temp60_ye40_ng24_ns3_Itemp10_Ieta10_version1.0_20140629.h5" -- NuLib opacity/emissivity table
 write_zones_every   = 1
 write_rays_every    = 1
 write_spectra_every = 1
@@ -27,31 +28,31 @@ nut_spec_n_phi            = 1          -- number of phi bins in output spectrum
 
 -- particle creation parameters
 
-n_emit_core    = 1e4               -- # particles to emit from core each timestep
+n_emit_core    = 1e6               -- # particles to emit from core each timestep
 n_emit_therm   = 0                 -- # particles to emit from zones each timestep
 n_emit_decay   = 0                 -- # particles to emit from zones each timestep (from non-thermal processes)
 max_particles  = 1e6
 
 -- particle propagation parameters
 
-max_n_steps = 10
+max_n_steps = 20
 dt = -1
 step_size = 0.4                    -- move at most step_size*min_grid_length at a time
 
 -- inner source
 
 r_core = 9e5                       -- core radius (cm)
-T_core = 1.6e11 -- 10MeV           -- core temperature (K)
-core_nue_chem_pot = -2e-5
+T_core = 10 -- 10MeV           -- core temperature (K)
+core_nue_chem_pot = 0
 core_lum_multiplier = 1.0
 
 -- opacity parameters
 
 nut_grey_opacity    =  -1          -- optical grey opacity (cm^2/g)
-nut_epsilon         =  -1          -- absorption fraction
+nut_grey_abs_frac   =  -1          -- absorption fraction
 
 -- equilibrium solver parameters
 
-damping         = 0.5              -- changes in values between iterations are decreased by this factor
+damping         = 0.1              -- changes in values between iterations are decreased by this factor
 brent_tolerance = 0.01             -- how close must the measured emission be to the predicted?
 brent_itmax     = 100              -- how many iterations of brent cycle (both inner and outer loop) are allowed
