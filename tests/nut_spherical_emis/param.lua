68562c1862bfce1f127bfd333f12aa771dc7a3a6
Sherwood Richers <srichers@caltech.edu>
2014-10-06 16:50:11 -0700

    update parfile for nut_spherical_emis

--- tests/nut_spherical_emis/param.lua
+++ tests/nut_spherical_emis/param.lua
@@ -1,60 +1,63 @@
 -- what are we simulating?
 
-verbose      = 1
-do_photons   = 0                 -- simulate photons?
-do_neutrinos = 1                 -- simulate neutrinos?
-radiative_eq = 0
-steady_state = 1                 -- iterative calculation (solve for steady-state configuration)? 
-solve_T      = 0                 -- (if iterative) solves each zone's temperature based on its absorbed energy
-solve_Ye     = 0                 -- (if iterative) solves each zone's Ye based on its absorbed lepton number
-do_visc = 0
+verbose       = 1
+do_photons    = 0
+do_neutrinos  = 1
+radiative_eq  = 0
+iterative     = 1
+solve_T       = 0
+solve_Ye      = 0
+do_visc       = 0
 reflect_outer = 0
 
 -- input/output files
 
-grid_type = "grid_1D_sphere"       -- grid geometry. Must match grid geometry in model file if used  
-model_file  =  "empty_sphere.mod"  -- model file. "custom" --> use hard coded model
-nulib_table = "../../external/tables/NuLib_LS220_rho150_temp90_ye60_ng24_ns3_Itemp10_Ieta10_version1.0_20140701.h5" -- NuLib opacity/emissivity table
+grid_type = "grid_1D_sphere"
+model_file  =  "empty_sphere.mod"
+nulib_table = "/data/tables/NuLib/NuLib_HShen_noscat.h5"
 write_zones_every   = 1
 write_rays_every    = 1
 write_spectra_every = 1
 
 -- spectrum parameters
 
-nut_spec_time_grid  = {1,1,1}          -- spectrum time grid parameters {start,stop,delta} (start==stop --> single bin catch-all)
-nut_spec_nu_grid    = {0,5e22,2e20}    -- spectrum frequency grid parameters {start,stop,delta} (start==stop --> single bin catch-all)
-nut_spec_n_mu       = 1                -- number of cos(theta) bins in output spectrum 
-nut_spec_n_phi      = 1                -- number of phi bins in output spectrum
+nut_spec_time_grid  = {1,1,1}
+nut_spec_nu_grid    = {0,5e22,2e20}
+nut_spec_n_mu       = 1
+nut_spec_n_phi      = 1
 
 -- particle creation parameters
 
-n_emit_core    = 1e7                 -- # particles to emit from core each timestep
-n_emit_therm   = 0                 -- # particles to emit from zones each timestep
-max_particles  = 1e7
+n_initial      = 0
+n_emit_core    = 1e6
+n_emit_therm   = 0
+max_particles  = 1e6
+emissions_per_timestep = 1
 
 -- particle propagation parameters
 
 max_n_steps = 1
 dt = -1
-step_size = 0.4                    -- move at most step_size*min_grid_length at a time
+step_size = 0.4
 
 -- inner source
 
-r_core = 1                         -- core radius (cm)
-T_core = 10                  -- core temperature (K)
-core_nue_chem_pot = 0           -- core chemical potential (erg)
+r_core = 1
+T_core = 10
+core_nue_chem_pot = 0
 core_lum_multiplier = 1.0
 
 -- opacity parameters
 
-nut_grey_opacity    =  -1 --0          -- optical grey opacity (cm^2/g)
-nut_grey_abs_frac   =  -1          -- absorption fraction
-nut_nugrid_start    = 1
-nut_nugrid_stop     = 2e10
-nut_nugrid_n=10
+nut_grey_opacity  = -1
+nut_grey_abs_frac = -1
+nut_nugrid_start  = 1
+nut_nugrid_stop   = 2e10
+nut_nugrid_n      = 10
+nut_cdf_cutoff    = 1e-2
 
 -- equilibrium solver parameters
 
-damping = 0.5                      -- changes in values between iterations are decreased by this factor
+damping = 0.5
 brent_itmax = 100
 brent_tolerance = 0.01
