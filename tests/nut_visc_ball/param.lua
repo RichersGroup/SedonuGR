23cca9ada724e246918b21954c1622e191377104
Sherwood Richers <srichers@caltech.edu>
2014-11-12 15:15:08 -0800

    updated nut_visc_ball test

--- tests/nut_visc_ball/param.lua
+++ tests/nut_visc_ball/param.lua
@@ -3,9 +3,10 @@
 verbose      = 1
 do_photons   = 0                 -- simulate photons?
 do_neutrinos = 1                 -- simulate neutrinos?
-steady_state = 1                 -- iterative calculation (solve for steady-state configuration)? 
+iterative    = 1                 -- iterative calculation (solve for steady-state configuration)? 
 radiative_eq = 0
 do_visc      = 1
+do_distribution = 0
 solve_T      = 1                 -- (if iterative) solves each zone's temperature based on its absorbed energy
 solve_Ye     = 1                 -- (if iterative) solves each zone's Ye based on its absorbed lepton number
 reflect_outer= 0
@@ -14,10 +15,10 @@ reflect_outer= 0
 
 grid_type = "grid_1D_sphere"       -- grid geometry. Must match grid geometry in model file if used  
 model_file  =  "neutron_star.mod"  -- model file. "custom" --> use hard coded model
-nulib_table = "../../external/tables/NuLib_LS220_rho150_temp90_ye60_ng24_ns3_Itemp10_Ieta10_version1.0_20140701.h5" -- NuLib opacity/emissivity table
-write_zones_every   = 1
-write_rays_every    = 1
-write_spectra_every = 1
+nulib_table = "/data/tables/NuLib/NuLib_HShen_fiducial.h5" -- NuLib opacity/emissivity table
+write_zones_every   = 10
+write_rays_every    = 10
+write_spectra_every = 10
 
 -- spectrum parameters
 
@@ -28,11 +29,16 @@ nut_spec_n_phi      = 1                -- number of phi bins in output spectrum
 
 -- particle creation parameters
 
+n_initial      = 0
 n_emit_core    = 0                 -- # particles to emit from core each timestep
 n_emit_therm   = 1e6                 -- # particles to emit from zones each timestep
 n_emit_decay   = 0                 -- # particles to emit from zones each timestep (from non-thermal processes)
 max_particles  = 1e6
+nut_cdf_cutoff = 1e-2
+cdf_interpolation_order = 1
 visc_specific_heat_rate = 1e22
+emissions_per_timestep = 1
+ratio_emit_by_zone = 0
 
 -- particle propagation parameters
 
@@ -40,10 +46,16 @@ max_n_steps = 10
 dt = -1
 step_size = 0.4                    -- move at most step_size*min_grid_length at a time
 
+
+r_core = 0
+T_core = 0
+core_nue_chem_pot = 0
+
 -- opacity parameters
 
 nut_grey_opacity    =  -1          -- optical grey opacity (cm^2/g)
 nut_grey_abs_frac   =  -1          -- absorption fraction
+opac_interp_method = 0
 
 -- equilibrium solver parameters
 
