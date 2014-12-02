37441ab6a2a7eee2677e77b6a6773ca13d000f7f
Sherwood Richers <srichers@caltech.edu>
2014-12-01 23:34:28 -0800

    make short_to_mod deal with data where first data point is at r=0

--- useful_scripts/short_to_mod.py
+++ useful_scripts/short_to_mod.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python                                                               
+#!/usr/bin/python
+# input assumes the short points are represented pointwise.
+# output radii are outer bin radii and fluid values are cell-averaged
 import sys
 
 from numpy import *
@@ -16,10 +18,15 @@ T_f   = data[:,1]
 rho_f = data[:,2]
 Ye_f  = data[:,3]
 print len(r_f)
-r   = np.ones( (len(r_f)-1) * interpolation_multiplier + 1 )
-T   = np.ones( (len(r_f)-1) * interpolation_multiplier + 1 )
-rho = np.ones( (len(r_f)-1) * interpolation_multiplier + 1 )
-Ye  = np.ones( (len(r_f)-1) * interpolation_multiplier + 1 )
+
+output_length = (len(r_f)-1) * interpolation_multiplier + 1
+if(r_f[0]==0):
+    output_length = output_length-1
+
+r   = np.ones(output_length)
+T   = np.ones(output_length)
+rho = np.ones(output_length)
+Ye  = np.ones(output_length)
 print len(r)
 
 # first zone is unchanged, constant rho,T,Ye
@@ -27,11 +34,19 @@ r[0]   =   r_f[0]
 rho[0] = rho_f[0]
 Ye[0]  =  Ye_f[0]
 T[0]   =   T_f[0]
+istart = 1
+if(r_f[0]==0):
+    r[0]   = r_f[1]
+    rho[0] = rho_f[1]
+    Ye[0]  = Ye_f[1]
+    T[0]   = T_f[1]
+    istart = 2
+
 
 # logarithmic interpolation
 rL = 0
 rR = 0
-for i in range(1,len(r_f)):
+for i in range(istart,len(r_f)):
     r_ratio   = pow(   r_f[i]/  r_f[i-1], 1.0/interpolation_multiplier)
     T_ratio   = pow(   T_f[i]/  T_f[i-1], 1.0/interpolation_multiplier)
     Ye_ratio  = pow(  Ye_f[i]/ Ye_f[i-1], 1.0/interpolation_multiplier)
@@ -44,6 +59,8 @@ for i in range(1,len(r_f)):
 
     for j in range(0,interpolation_multiplier):
         index = 1 + j + (i-1)*interpolation_multiplier
+        if(r_f[0]==0):
+            index = index-1
         rL   =   r_f[i-1] * pow(  r_ratio,j)
         TL   =   T_f[i-1] * pow(  T_ratio,j)
         YeL  =  Ye_f[i-1] * pow( Ye_ratio,j)
@@ -51,7 +68,7 @@ for i in range(1,len(r_f)):
 
         # ratios are defined assuming conservative variables (rho,eps,Ye) scale as some power of r. Assume eps = nkT.
         # integrated by hand to average over cell volume
-        r[index]   = r_f[i-1] * pow(r_ratio, j+1)
+        r[index]   = rL * r_ratio 
         Ye[index]  = 3* YeL/(3+ Ye_power) * (pow(r_ratio, Ye_power+3)-1) / (pow(r_ratio,3)-1)
         rho[index] = 3*rhoL/(3+rho_power) * (pow(r_ratio,rho_power+3)-1) / (pow(r_ratio,3)-1)
         T[index]   = 3*  TL/(3+eps_power) * (pow(r_ratio,eps_power+3)-1) / (pow(r_ratio,3)-1) * rhoL/rho[index]
