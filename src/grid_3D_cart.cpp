58b3d14919b425d0734b1bc42934b846348613e9
Sherwood Richers <srichers@caltech.edu>
2015-07-07 04:05:14 -0700

    added license to top of source files

--- src/grid_3D_cart.cpp
+++ src/grid_3D_cart.cpp
@@ -1,3 +1,30 @@
+/*
+//  Copyright (c) 2015, California Institute of Technology and the Regents
+//  of the University of California, based on research sponsored by the
+//  United States Department of Energy. All rights reserved.
+//
+//  This file is part of Sedonu.
+//
+//  Sedonu is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Neither the name of the California Institute of Technology (Caltech)
+//  nor the University of California nor the names of its contributors 
+//  may be used to endorse or promote products derived from this software
+//  without specific prior written permission.
+//
+//  Sedonu is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Sedonu.  If not, see <http://www.gnu.org/licenses/>.
+//
+*/
+
 #include "grid_3D_cart.h"
 #include <cmath>
 #include <cstdlib>
