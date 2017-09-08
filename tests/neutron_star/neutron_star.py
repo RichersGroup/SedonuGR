4e6c3d77c00e6b9b20d5a525aea6eabb3c092fa9
Sherwood Richers <srichers@caltech.edu>
2017-09-08 12:33:38 -0400

    updated neutron star test

--- tests/neutron_star/neutron_star.py
+++ tests/neutron_star/neutron_star.py
@@ -11,8 +11,8 @@ ye_max = 0.2
 
 temp = 5*MeV
 
-print '1D_sphere',nx,R_min
+print('1D_sphere',nx,R_min)
 
 for i  in range(1,nx+1):
     R = R_min + i*dx
-    print R, rho_max*(1-(R/R_max)**2), temp, ye_max #*(1-(R/R_max)**2)+.05
+    print(R, rho_max*(1-(R/R_max)**2), temp, ye_max,0,0,0) #*(1-(R/R_max)**2)+.05
