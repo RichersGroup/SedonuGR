834c81fc2718560352330e86542ad7eebf13a260
Sherwood Richers <srichers@caltech.edu>
2018-01-04 22:46:03 -0500

    whitespace

--- src/misc/EinsteinHelper.h
+++ src/misc/EinsteinHelper.h
@@ -11,149 +11,149 @@ using namespace std;
 
 class EinsteinHelper{
 public:
-  // essential variables interpolated from grid
-  Particle p;
-  Metric g;
-  Christoffel christoffel;
-  double u[4]; // dimensionless
+	// essential variables interpolated from grid
+	Particle p;
+	Metric g;
+	Christoffel christoffel;
+	double u[4]; // dimensionless
 
-  // intermediate quantities
-  double e[4][4];
-  double kup_tet[4];
-  double absopac, scatopac;
-  double ds_com;
-  unsigned dir_ind[NDIMS+1]; // spatial, nu_in
-  int z_ind, eas_ind;   // direct access indices
+	// intermediate quantities
+	double e[4][4];
+	double kup_tet[4];
+	double absopac, scatopac;
+	double ds_com;
+	unsigned dir_ind[NDIMS+1]; // spatial, nu_in
+	int z_ind, eas_ind;   // direct access indices
 
-  EinsteinHelper(){
-	  for(unsigned i=0; i<4; i++){
-		  u[i] = NaN;
-		  kup_tet[i] = NaN;
-		  for(unsigned j=0; j<4; j++)
-			  e[i][j] = NaN;
-	  }
-	  absopac = NaN;
-	  scatopac = NaN;
-	  ds_com = NaN;
-	  z_ind = -MAXLIM;
-	  eas_ind = -MAXLIM;
-	  for(unsigned i=0; i<NDIMS+1; i++)
-		  dir_ind[i] = MAXLIM;
-  }
+	EinsteinHelper(){
+		for(unsigned i=0; i<4; i++){
+			u[i] = NaN;
+			kup_tet[i] = NaN;
+			for(unsigned j=0; j<4; j++)
+				e[i][j] = NaN;
+		}
+		absopac = NaN;
+		scatopac = NaN;
+		ds_com = NaN;
+		z_ind = -MAXLIM;
+		eas_ind = -MAXLIM;
+		for(unsigned i=0; i<NDIMS+1; i++)
+			dir_ind[i] = MAXLIM;
+	}
 
-  // fill in values for g.{gtt, betalow}, u, e, kup_tet
-  // assumes g.{alpha, betaup, gammalow}, p.kup are set
-  void update(const double v[3]){ // use v as input since we don't store it
-    g.update();
-    set_fourvel(v);
-    set_tetrad_basis(u);
-    coord_to_tetrad(p.kup, kup_tet);
-    g.normalize_null(p.kup);
-  }
-  
-  // return the Lorentz factor W
-  double lorentzFactor(const double v[3]) const{
-    return 1. / sqrt(1. - g.dot<3>(v,v));
-  }
-  
-  double nu() const{
-	  double nu = kup_tet[3] / pc::h;
-	  PRINT_ASSERT(nu,>,0);
-	  return nu;
-  }
+	// fill in values for g.{gtt, betalow}, u, e, kup_tet
+	// assumes g.{alpha, betaup, gammalow}, p.kup are set
+	void update(const double v[3]){ // use v as input since we don't store it
+		g.update();
+		set_fourvel(v);
+		set_tetrad_basis(u);
+		coord_to_tetrad(p.kup, kup_tet);
+		g.normalize_null(p.kup);
+	}
 
-  // get four velocity from three velocity
-  void set_fourvel(const double v[3]){
-    double W = lorentzFactor(v);
-    u[3] = W/g.alpha;
-    for(unsigned i=0; i<3; i++)
-      u[i] = v[3]/pc::c * (g.alpha*v[i]/pc::c - g.betaup[i]);
-  }
+	// return the Lorentz factor W
+	double lorentzFactor(const double v[3]) const{
+		return 1. / sqrt(1. - g.dot<3>(v,v));
+	}
+
+	double nu() const{
+		double nu = kup_tet[3] / pc::h;
+		PRINT_ASSERT(nu,>,0);
+		return nu;
+	}
+
+	// get four velocity from three velocity
+	void set_fourvel(const double v[3]){
+		double W = lorentzFactor(v);
+		u[3] = W/g.alpha;
+		for(unsigned i=0; i<3; i++)
+			u[i] = v[3]/pc::c * (g.alpha*v[i]/pc::c - g.betaup[i]);
+	}
 
 
-  // get a Cartesian tetrad basis
-  void set_tetrad_basis(const double u[4]){
-    // normalize four-velocity to get timelike vector
-    for(int mu=0; mu<4; mu++) e[3][mu] = u[mu];
-    g.normalize(e[3]);
-    
-    // use x0 as a trial vector
-    e[0][0] = 1.0;
-    e[0][1] = 0;
-    e[0][2] = 0;
-    e[0][3] = 0;
-    g.orthogonalize<4>(e[0],e[3]);
-    g.normalize(e[0]);
-    
-    // use x1 as a trial vector
-    e[1][0] = 0;
-    e[1][1] = 1.0;
-    e[1][2] = 0;
-    e[1][3] = 0;
-    g.orthogonalize<4>(e[1],e[3]);
-    g.orthogonalize<4>(e[1],e[0]);
-    g.normalize(e[1]);
-    
-    // use x2 as a trial vector
-    e[2][0] = 0;
-    e[2][1] = 0;
-    e[2][2] = 1.0;
-    e[2][3] = 0;
-    g.orthogonalize<4>(e[2],e[3]);
-    g.orthogonalize<4>(e[2],e[0]);
-    g.orthogonalize<4>(e[2],e[1]);
-    g.normalize(e[2]);
-    
-    // sanity checks
-    PRINT_ASSERT(fabs(g.dot<4>(e[0],e[1])),<,TINY);
-    PRINT_ASSERT(fabs(g.dot<4>(e[0],e[2])),<,TINY);
-    PRINT_ASSERT(fabs(g.dot<4>(e[0],e[3])),<,TINY);
-    PRINT_ASSERT(fabs(g.dot<4>(e[1],e[2])),<,TINY);
-    PRINT_ASSERT(fabs(g.dot<4>(e[1],e[3])),<,TINY);
-    PRINT_ASSERT(fabs(g.dot<4>(e[2],e[3])),<,TINY);
-  }
+	// get a Cartesian tetrad basis
+	void set_tetrad_basis(const double u[4]){
+		// normalize four-velocity to get timelike vector
+		for(int mu=0; mu<4; mu++) e[3][mu] = u[mu];
+		g.normalize(e[3]);
 
-  void coord_to_tetrad(const double kup_coord[4], double kup_tet[4]) const{
-	  for(int mu=0; mu<4; mu++) kup_tet[mu] = g.dot<4>(kup_coord,e[mu]);
-	  kup_tet[3] *= -1.;
-  }
+		// use x0 as a trial vector
+		e[0][0] = 1.0;
+		e[0][1] = 0;
+		e[0][2] = 0;
+		e[0][3] = 0;
+		g.orthogonalize<4>(e[0],e[3]);
+		g.normalize(e[0]);
 
-  void tetrad_to_coord(const double kup_tet[4], double kup_coord[4]) const{
-	  for(int mu=0; mu<4; mu++){
-		  kup_coord[mu] = 0;
-		  for(int nu=0; nu<4; nu++)
-			  kup_coord[mu] += kup_tet[nu] * e[nu][mu];
-	  }
-  }
+		// use x1 as a trial vector
+		e[1][0] = 0;
+		e[1][1] = 1.0;
+		e[1][2] = 0;
+		e[1][3] = 0;
+		g.orthogonalize<4>(e[1],e[3]);
+		g.orthogonalize<4>(e[1],e[0]);
+		g.normalize(e[1]);
 
-  template<unsigned n>
-  double dot_tetrad(const double x1[n], const double x2[n]){
-	  double result = 0;
-	  for(unsigned i=0; i<3; i++) result += x1[n]*x2[n];
-	  if(n==4) result -= x1[2]*x2[3];
-	  return result;
-  }
+		// use x2 as a trial vector
+		e[2][0] = 0;
+		e[2][1] = 0;
+		e[2][2] = 1.0;
+		e[2][3] = 0;
+		g.orthogonalize<4>(e[2],e[3]);
+		g.orthogonalize<4>(e[2],e[0]);
+		g.orthogonalize<4>(e[2],e[1]);
+		g.normalize(e[2]);
 
-  void scale_p_frequency(const double scale){
-	  for(unsigned i=0; i<4; i++) p.kup[i] *= scale;
-  }
+		// sanity checks
+		PRINT_ASSERT(fabs(g.dot<4>(e[0],e[1])),<,TINY);
+		PRINT_ASSERT(fabs(g.dot<4>(e[0],e[2])),<,TINY);
+		PRINT_ASSERT(fabs(g.dot<4>(e[0],e[3])),<,TINY);
+		PRINT_ASSERT(fabs(g.dot<4>(e[1],e[2])),<,TINY);
+		PRINT_ASSERT(fabs(g.dot<4>(e[1],e[3])),<,TINY);
+		PRINT_ASSERT(fabs(g.dot<4>(e[2],e[3])),<,TINY);
+	}
 
-  double ds_lab(const double ds_com_in){
-	  return -ds_com_in * g.ndot(p.kup) / (pc::h*nu());
-  }
+	void coord_to_tetrad(const double kup_coord[4], double kup_tet[4]) const{
+		for(int mu=0; mu<4; mu++) kup_tet[mu] = g.dot<4>(kup_coord,e[mu]);
+		kup_tet[3] *= -1.;
+	}
 
-  void integrate_geodesic(){
-  	double dlambda = ds_com / (pc::h*nu());
+	void tetrad_to_coord(const double kup_tet[4], double kup_coord[4]) const{
+		for(int mu=0; mu<4; mu++){
+			kup_coord[mu] = 0;
+			for(int nu=0; nu<4; nu++)
+				kup_coord[mu] += kup_tet[nu] * e[nu][mu];
+		}
+	}
 
-  	double dk_dlambda[4] = {0,0,0,0};
-  	if(DO_GR) christoffel.contract2(p.kup,dk_dlambda);
+	template<unsigned n>
+	double dot_tetrad(const double x1[n], const double x2[n]){
+		double result = 0;
+		for(unsigned i=0; i<3; i++) result += x1[n]*x2[n];
+		if(n==4) result -= x1[2]*x2[3];
+		return result;
+	}
 
-  	// get new x,k
-  	for(int i=0; i<4; i++){
-  		p.kup[i] += dk_dlambda[i]*dlambda;
-  		p.xup[i] +=      p.kup[i]*dlambda;
-  	}
-  }
+	void scale_p_frequency(const double scale){
+		for(unsigned i=0; i<4; i++) p.kup[i] *= scale;
+	}
+
+	double ds_lab(const double ds_com_in){
+		return -ds_com_in * g.ndot(p.kup) / (pc::h*nu());
+	}
+
+	void integrate_geodesic(){
+		double dlambda = ds_com / (pc::h*nu());
+
+		double dk_dlambda[4] = {0,0,0,0};
+		if(DO_GR) christoffel.contract2(p.kup,dk_dlambda);
+
+		// get new x,k
+		for(int i=0; i<4; i++){
+			p.kup[i] += dk_dlambda[i]*dlambda;
+			p.xup[i] +=      p.kup[i]*dlambda;
+		}
+	}
 
 };
 
--- src/misc/Metric.h
+++ src/misc/Metric.h
@@ -50,106 +50,106 @@ class Metric{
 private:
 	double gtt, betalow[3];
 
- public:
-  double alpha, betaup[3];
-  ThreeMetric gammalow;
+public:
+	double alpha, betaup[3];
+	ThreeMetric gammalow;
 
-  Metric(){
-	  gtt=NaN;
-	  alpha = NaN;
-	  for(unsigned i=0; i<3; i++){
-		  betaup[i] = NaN;
-		  betalow[i] = NaN;
-	  }
-  }
+	Metric(){
+		gtt=NaN;
+		alpha = NaN;
+		for(unsigned i=0; i<3; i++){
+			betaup[i] = NaN;
+			betalow[i] = NaN;
+		}
+	}
 
-  // fill in values for gtt and betalow
-  // assumes alpha, betaup, and gammalow have been set.
-  void update(){
-    lower<3>(betaup, betalow);
-    gtt = -alpha*alpha + contract<3>(betaup, betalow);
-  }
+	// fill in values for gtt and betalow
+	// assumes alpha, betaup, and gammalow have been set.
+	void update(){
+		lower<3>(betaup, betalow);
+		gtt = -alpha*alpha + contract<3>(betaup, betalow);
+	}
 
-  template<unsigned n>
-  void lower(const double xup[], double xdown[]) const{
-    xdown[0] = gammalow.xx*xup[0] + gammalow.xy*xup[1] + gammalow.xz*xup[2];
-    xdown[1] = gammalow.xy*xup[0] + gammalow.yy*xup[1] + gammalow.yz*xup[2];
-    xdown[2] = gammalow.xz*xup[0] + gammalow.yz*xup[1] + gammalow.zz*xup[2];
+	template<unsigned n>
+	void lower(const double xup[], double xdown[]) const{
+		xdown[0] = gammalow.xx*xup[0] + gammalow.xy*xup[1] + gammalow.xz*xup[2];
+		xdown[1] = gammalow.xy*xup[0] + gammalow.yy*xup[1] + gammalow.yz*xup[2];
+		xdown[2] = gammalow.xz*xup[0] + gammalow.yz*xup[1] + gammalow.zz*xup[2];
 
-    if(n==4){
-      xdown[3]  = xup[3] * gtt;
-      for(unsigned i=0; i<3; i++){
-	xdown[3] += xup[i] * betalow[i];
-	xdown[i] += xup[3] * betalow[i];
-      }
-    }
-  }
+		if(n==4){
+			xdown[3]  = xup[3] * gtt;
+			for(unsigned i=0; i<3; i++){
+				xdown[3] += xup[i] * betalow[i];
+				xdown[i] += xup[3] * betalow[i];
+			}
+		}
+	}
 
-  template<unsigned n>
-    double contract(const double xup[n], const double xdown[n]) const{
-    double result = 0;
-    for(int i=0; i<n; i++)result += xup[i]*xdown[i];
-    return result;
-  }
-        
-  // dot product
-  template<unsigned n>
-  double dot(const double x1up[n], const double x2up[n]) const{
-    double x2low[n];
-    lower<n>(x2up, x2low);
-    double result = contract<n>(x1up, x2low);
-    return result;
-  }
+	template<unsigned n>
+	double contract(const double xup[n], const double xdown[n]) const{
+		double result = 0;
+		for(int i=0; i<n; i++)result += xup[i]*xdown[i];
+		return result;
+	}
 
-  // dot the normal observer's four-velocity with a four vector
-  double ndot(const double x[4]) const{
-    return -alpha * x[3];
-  }
+	// dot product
+	template<unsigned n>
+	double dot(const double x1up[n], const double x2up[n]) const{
+		double x2low[n];
+		lower<n>(x2up, x2low);
+		double result = contract<n>(x1up, x2low);
+		return result;
+	}
 
-  // normalize a four vector to have a norm of +/-1
-  void normalize(double x[4]) const{
-    double invnorm = fabs(1./dot<4>(x,x));
-    for(int i=0; i<4; i++) x[i] *= invnorm;
-  }
+	// dot the normal observer's four-velocity with a four vector
+	double ndot(const double x[4]) const{
+		return -alpha * x[3];
+	}
 
-  // make a vector null
-  void normalize_null(double x[4]) const{
-    const double invA = 1./gtt;
-    const double B = (x[0]*betalow[0] + x[1]*betalow[1] + x[2]*betalow[2]) * invA;
-    const double C = dot<3>(x,x) * invA;
-    const double result = 0.5 * (B + sqrt(B*B - 4.*C));
-    PRINT_ASSERT(result,>,0);
-    x[3] = result;
-  }
+	// normalize a four vector to have a norm of +/-1
+	void normalize(double x[4]) const{
+		double invnorm = fabs(1./dot<4>(x,x));
+		for(int i=0; i<4; i++) x[i] *= invnorm;
+	}
 
-  // make four vector v orthogonal to four vector v
-  template<unsigned n>
-  void orthogonalize(double v[n], const double e[n]) const{
-    double projection = dot<n>(v,e);
-    for(int mu=0; mu<n; mu++) v[mu] -= projection * e[mu];
-  }  
+	// make a vector null
+	void normalize_null(double x[4]) const{
+		const double invA = 1./gtt;
+		const double B = (x[0]*betalow[0] + x[1]*betalow[1] + x[2]*betalow[2]) * invA;
+		const double C = dot<3>(x,x) * invA;
+		const double result = 0.5 * (B + sqrt(B*B - 4.*C));
+		PRINT_ASSERT(result,>,0);
+		x[3] = result;
+	}
 
-  // vector operations
-  template<unsigned s>
-  static double dot_Minkowski(const double a[], const double b[]){
-  	double product = 0;
-  	for(unsigned i=0; i<3; i++) product += a[i]*b[i];
-  	if(s==4) product -= a[3]*b[3];
-  	return product;
-  }
+	// make four vector v orthogonal to four vector v
+	template<unsigned n>
+	void orthogonalize(double v[n], const double e[n]) const{
+		double projection = dot<n>(v,e);
+		for(int mu=0; mu<n; mu++) v[mu] -= projection * e[mu];
+	}
 
-  // normalize a vector
-  template<unsigned s>
-  static void normalize_Minkowski(double a[]){
-  	double inv_magnitude = 1./sqrt(fabs( dot_Minkowski<s>(a,a) ));
-  	PRINT_ASSERT(inv_magnitude,<,INFINITY);
-  	for(unsigned i=0; i<s; i++) a[i] *= inv_magnitude;
-  }
+	// vector operations
+	template<unsigned s>
+	static double dot_Minkowski(const double a[], const double b[]){
+		double product = 0;
+		for(unsigned i=0; i<3; i++) product += a[i]*b[i];
+		if(s==4) product -= a[3]*b[3];
+		return product;
+	}
 
-  static void normalize_null_Minkowski(double a[4]){
-  	double spatial_norm = dot_Minkowski<3>(a,a);
-  	a[3] = sqrt(spatial_norm);
-  }
+	// normalize a vector
+	template<unsigned s>
+	static void normalize_Minkowski(double a[]){
+		double inv_magnitude = 1./sqrt(fabs( dot_Minkowski<s>(a,a) ));
+		PRINT_ASSERT(inv_magnitude,<,INFINITY);
+		for(unsigned i=0; i<s; i++) a[i] *= inv_magnitude;
+	}
+
+	static void normalize_null_Minkowski(double a[4]){
+		double spatial_norm = dot_Minkowski<3>(a,a);
+		a[3] = sqrt(spatial_norm);
+	}
 
 };
 
