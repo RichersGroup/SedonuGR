0c6821e96b51d16c7c6865e775178201e2d540ca
Sherwood Richers <srichers@caltech.edu>
2018-02-15 09:57:14 -0500

    tiny (semicolon)

--- src/species/nulib_interface.h
+++ src/species/nulib_interface.h
@@ -43,7 +43,7 @@ void nulib_get_eas_arrays(double rho, double temp, double ye, int nulibID,
 void nulib_get_epannihil_kernels(
 		const double rho, const double temp, const double ye, const int nulibID,
 		vector< vector<double> >& phi0,
-		vector< vector<double> >& phi1)
+		vector< vector<double> >& phi1);
 void nulib_get_pure_emis(double rho, double temp, double ye, int nulibID, vector<double>& pure_emis);
 void nulib_get_nu_grid(Axis& nut_nu_grid);
 int nulib_get_nspecies();
