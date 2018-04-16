219636e521713c84efd8d6d0bfc3f4081ca4cc8e
Sherwood Richers <srichers@caltech.edu>
2018-04-16 14:02:59 -0400

    neutron star test works again

--- tests/neutron_star/param.lua
+++ tests/neutron_star/param.lua
@@ -5,6 +5,7 @@ do_relativity = 0
 do_annihilation = 0
 radiative_eq  = 1
 reflect_outer = 0
+use_scattering_kernels = 0
 
 -- Equilibrium Solving
 
@@ -53,33 +54,35 @@ output_hdf5 = 0
 
 max_particles  = 2e7
 n_subcycles = 1
-do_emit_by_bin = 0
-n_emit_core    = 1e5
-n_emit_therm   = 0
+n_emit_core_per_bin    = 1e3
+n_emit_therm_per_bin   = 0
 
 -- Inner Source
 
 r_core = 9e5
 core_emit_method = 1
-T_core = 10
-core_nue_chem_pot = 0
-core_lum_multiplier = 1.0
+T_core = {10,10,10}
+core_chem_pot = {0,0,0}
+core_lum_multiplier = {1.0,1.0,1.0}
 
 -- General Controls
 
 verbose       = 1
 max_n_iter =  20
-step_size = 0.01
+min_step_size = 0.01
+max_step_size = 0.1
 max_time_hours = -1
 
 -- Biasing
 
-importance_bias = 0
 bias_path_length = 0
-min_packet_energy = 1e24
-max_packet_energy = 1e99
-exponential_decay = 0
+min_packet_weight = 0.1
 
 -- Random Walk
 
-randomwalk_sphere_size = 0
+do_randomwalk = 1
+randomwalk_max_x = 2
+randomwalk_sumN = 1000
+randomwalk_npoints = 200
+randomwalk_min_optical_depth = 100
+randomwalk_interpolation_order = 1
