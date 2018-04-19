8bf548f8eacee8bd3b5ea8a0a84b8d86e08d6f23
Sherwood Richers <srichers@caltech.edu>
2018-04-19 18:15:41 -0400

    logarithmic grid for schwarzschild path test

--- tests/schwarzschild_path_test/empty_sphere.py
+++ tests/schwarzschild_path_test/empty_sphere.py
@@ -1,19 +1,20 @@
 import numpy as np
-nx = 9*8
+nx = 9*4
 rs = 1.0
-rin = 1
+Rin = 1
+Rout = 10
 
-R = 10
 rho = 0
 temp = 0
 Ye = 0
 vr=0
 
-print('1D_sphere', nx,rin)
-dr = (R-rin)/nx
+print('1D_sphere', nx,Rin)
+rfac = (Rout/Rin)**(1./nx)
 for i in range(nx):
-    rout = rin + dr*(i+1  )
-    rmid = rin + dr*(i+0.5)
+    rin  = Rin * rfac**i
+    rout = Rin * rfac**(i+1)
+    rmid = 0.5 * (rin+rout)
     alpha = np.sqrt(1.0-rs/rmid)
     X = 1.0/alpha
     print(rout, rho, temp, Ye, vr, alpha, X)
