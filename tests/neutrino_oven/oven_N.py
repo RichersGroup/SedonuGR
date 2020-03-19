fb0f40c971ad9ca89d71b39675d5a90a55be5e6b
Sherwood Richers <srichers@caltech.edu>
2020-03-18 21:42:46 -0700

    finished improving and automating neutrino_oven test - now it is a filled sphere rather than an emitting inner source

--- tests/neutrino_oven/oven_N.py
+++ tests/neutrino_oven/oven_N.py
@@ -3,10 +3,10 @@ import sys
 sys.path.insert(0, '../')
 import tools
 
-nx = 10
+nx = 31
 rs = 1.0e5
-Rin = 1.5e5
-Rout = 1.75e5
+Rcore = 1.5e5
+Rout = 3e5 #1.75 --2.0e5
 
 rho = 1
 temp_core = 10*tools.MeV / tools.k_b
@@ -16,15 +16,18 @@ vr=0
 def lapse(r):
     return np.sqrt(1.0 - rs/r)
 
-print('1D_sphere', nx,Rin)
-rfac = (Rout/Rin)**(1./nx)
-alpha_core = lapse(Rin)
-for i in range(nx):
-    rin  = Rin * rfac**i
-    rout = Rin * rfac**(i+1)
+print('1D_sphere', nx-2,0)
+dr = Rout/nx
+alpha_core = lapse(Rcore)
+for i in range(2,nx):
+    rin  = dr*i
+    rout = dr*(i+1)
     rmid = 0.5 * (rin+rout)
-    alpha = lapse(rmid)
-    X = 1.0 /alpha
+    if(rout<=Rcore):
+        alpha = alpha_core
+    else:
+        alpha = lapse(rmid)
+    X = 1.0/alpha
     temp = temp_core #* alpha_core/alpha
     print(rout, rho, temp, Ye, vr, alpha, X)
 
--- tests/neutrino_oven/oven_test.py
+++ tests/neutrino_oven/oven_test.py
@@ -4,13 +4,26 @@ if makeplot:
     mpl.use('Agg')
     import matplotlib.pyplot as plt
     import matplotlib.gridspec as gridspec
+    mpl.rcParams['font.size'] = 16
+    mpl.rcParams['font.family'] = 'serif'
+    mpl.rcParams['xtick.major.size'] = 7
+    mpl.rcParams['xtick.major.width'] = 2
+    mpl.rcParams['xtick.major.pad'] = 8
+    mpl.rcParams['xtick.minor.size'] = 4
+    mpl.rcParams['xtick.minor.width'] = 2
+    mpl.rcParams['ytick.major.size'] = 7
+    mpl.rcParams['ytick.major.width'] = 2
+    mpl.rcParams['ytick.minor.size'] = 4
+    mpl.rcParams['ytick.minor.width'] = 2
+    mpl.rcParams['axes.linewidth'] = 2
 import h5py
 import numpy as np
 import sys
 sys.path.insert(0, '../')
 import tools
 
-tolerance = 0.05
+avg_tolerance = 0.05
+max_tolerance = 0.5
 
 with open("param.lua") as search:
     for line in search:
@@ -29,68 +42,70 @@ with open("output.txt","r") as search:
 f = h5py.File("fluid_00001.h5","r")
 r = np.array(f["axes/x0(cm)[mid]"])/1e5
 T_gas = np.array(f["T_gas(K,tet)"])*tools.k_b/tools.MeV
-edens = np.array(f["distribution0(erg|ccm,tet)"]).sum(axis=(1,2,3))
-edens_theory = np.array([tools.edens(T_gas[i]*tools.MeV, munue*tools.MeV) for i in range(len(T_gas))])
+edens = np.array(f["distribution0(erg|ccm,tet)"]).sum(axis=(1,2,3))/1e29
+edens_theory = np.array([tools.edens(T_gas[i]*tools.MeV, munue*tools.MeV) for i in range(len(T_gas))])/1e29
 #edens_theory = np.array([tools.energy_blackbody(T_gas[i]*tools.MeV, munue*tools.MeV) for i in range(len(T_gas))])
 if makeplot:
     fig = plt.figure(figsize=(8,12))
     gs = gridspec.GridSpec(3, 1)
     axes = [plt.subplot(gsi) for gsi in gs]
-    axes[0].set_title(r"$R_\mathrm{core}=$"+str(r_core)+r"km   $T_\mathrm{core}=$"+str(T_core)+r"MeV   $\mu_{\nu_e}=$"+str(munue/tools.MeV)+"MeV")
+    #axes[0].set_title(r"$R_\mathrm{core}=$"+str(r_core)+r"km   $T_\mathrm{core}=$"+str(T_core)+r"MeV   $\mu_{\nu_e}=$"+str(munue/tools.MeV)+"MeV")
 
 isError = False
 
 if makeplot:
-    axes[0].set_ylabel(r"Energy Density (erg/ccm)")
-    axes[0].plot(r, edens_theory, "g-",label="theory")
+    axes[0].set_ylabel(r"$E$ ($10^{29}$ erg cm$^{-3}$)")
+    axes[0].plot(r, edens_theory, "g-",label="Theory")
     axes[0].scatter(r, edens, label="Sedonu")
-    axes[0].legend(loc=4)
+    axes[0].legend(frameon=False)
     axes[0].set_ylim(0,1.1*max(np.max(edens),np.max(edens_theory)))
 error = edens-edens_theory
-error2 = (edens-edens_theory)**2
-std_dev = np.sqrt(np.sum(error2)/len(error2) - (np.sum(error)/len(error))**2)
-relerror = np.sum(np.abs(error)/edens_theory)/len(error)
-print("Energy density: error="+str(relerror)+" stddev="+str(std_dev))
-if relerror > 0.05:
+relerror = error/edens_theory
+avgerror = np.abs(np.mean(relerror))
+maxerror = np.max(np.abs(relerror))
+print("Energy density avgerror=",avgerror," maxerror=",maxerror)
+if avgerror>avg_tolerance or maxerror>max_tolerance:
     isError = True
-
-e_abs = np.array(f["four-force[abs](erg|ccm|s,tet)"])[:,3]
-e_emit = np.array(f["four-force[emit](erg|ccm|s,tet)"])[:,3]
+    
+e_abs = np.array(f["four-force[abs](erg|ccm|s,tet)"])[:,3]/1e30
+e_emit = np.array(f["four-force[emit](erg|ccm|s,tet)"])[:,3]/1e30
 dEdt = e_abs + e_emit
 if makeplot:
-    axes[1].set_ylabel(r"$dE_\mathrm{int}/dt$ (erg/ccm/s)")
-    axes[1].plot(r, -e_emit, label="emit")
-    axes[1].scatter(r, e_abs, label="abs")
-    axes[1].legend(loc=4)
+    axes[1].set_ylabel(r"$dE_\mathrm{int}/dt$ ($10^{30}$ erg cm$^{-3}$ s$^{-1}$)")
+    axes[1].plot(r, -e_emit, label="Emit")
+    axes[1].scatter(r, e_abs, label="Absorb")
+    axes[1].legend(frameon=False)
     axes[1].set_ylim(0,1.1*max(np.max(e_abs),np.max(-e_emit)))
 error = dEdt
-error2 = dEdt**2
-std_dev = np.sqrt(np.sum(error2)/len(error2) - (np.sum(error)/len(error))**2)
-abserror = np.sum(np.abs(error))/len(error)
-print("dEdt: error="+str(abserror)+" stddev="+str(std_dev))
-if std_dev>0 and abserror>3.*std_dev:
+relerror = error / e_emit
+avgerror = np.abs(np.mean(relerror))
+maxerror = np.max(np.abs(relerror))
+print("dEdt avgerror=",avgerror," maxerror=",maxerror)
+if avgerror>avg_tolerance or maxerror>max_tolerance:
     isError = True
 
-l_abs = np.array(f["l_abs(1|s|ccm,tet)"])
-l_emit = np.array(f["l_emit(1|s|ccm,tet)"])
+l_abs = np.array(f["l_abs(1|s|ccm,tet)"])/1e35
+l_emit = np.array(f["l_emit(1|s|ccm,tet)"])/1e35
 dNdt = l_abs + l_emit
 if makeplot:
-    axes[2].set_ylabel(r"$dn_l/dt$ (1/ccm/s)")
-    axes[2].plot(r, -l_emit, label="emit")
-    axes[2].scatter(r, l_abs, label="abs")
+    axes[2].set_ylabel(r"$dn_l/dt$ ($10^{35}$ cm$^{-3}$ s$^{-1}$)")
+    axes[2].plot(r, -l_emit, label="Emit")
+    axes[2].scatter(r, l_abs, label="Absorb")
     axes[2].set_ylim(0,1.1*max(np.max(l_abs),np.max(-l_emit)))
-    axes[2].legend()
+    axes[2].legend(frameon=False)
 error = dNdt
-error2 = dNdt**2
-std_dev = np.sqrt(np.sum(error2)/len(error2) - (np.sum(error)/len(error))**2)
-abserror = np.sum(np.abs(error))/len(error)
-print("dNdt: error="+str(abserror)+" stddev="+str(std_dev))
-if std_dev>0 and abserror>3.*std_dev:
+relerror = dNdt / l_emit
+avgerror = np.abs(np.mean(relerror))
+maxerror = np.max(np.abs(relerror))
+print("dEdt avgerror=",avgerror," maxerror=",maxerror)
+if avgerror>avg_tolerance or maxerror>max_tolerance:
     isError = True
 
 if makeplot:
     for ax in axes:
+        ax.minorticks_on()
         ax.axvline(1.5)
+        ax.tick_params(axis='both',which='both',direction='in',top=True,right=True)
     
     axes[2].set_xlabel("$r$ (km)")
     axes[0].xaxis.set_ticklabels([])
